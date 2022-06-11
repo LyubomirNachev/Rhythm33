a19305900df5676bcde2fb1ed12387f3c67b7c05
Lyubomir Nachev <48567009+LyubomirNachev@users.noreply.github.com>
2022-06-11 18:05:46 +0300

    updated test program
    The old mp3 test UI program, but with design file separation, much cleaner

--- test_kiwi.py
+++ test_kiwi.py
@@ -4,51 +4,18 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 from pytube import YouTube
+from kivy.properties import ObjectProperty
 import subprocess
 import os
 
-class MyGridLayout(GridLayout):
-	# Initialize infinite keywords
-	def __init__(self, **kwargs):
-		# Call grid layout constructor
-		super(MyGridLayout, self).__init__(**kwargs)
+class MyGridLayout(Widget):
 
-		# Set columns
-		self.cols = 2
+	name = ObjectProperty(None)
+	download = ObjectProperty(None)
 
-		# Create a second gridlayout
-		self.top_grid = GridLayout()
-		self.top_grid.cols = 1
-
-		# Add widgets
-		self.top_grid.add_widget(Label(text="Download: ", font_size=16))
-		# Add Input Box
-		self.download = TextInput(multiline=False,
-			size_hint_y=None,
-			height=30,
-			size_hint_x=None,
-			width=900
-			)
-		self.top_grid.add_widget(self.download)
-
-		#Add the new top_grid to our app
-		self.add_widget(self.top_grid)
-
-
-		# Create a Submit Button
-		self.submit = Button(text="Enter",
-			font_size=16,
-			size_hint_y=None,
-			height=50,
-			size_hint_x=None,
-			width=100
-			)
-		# Bind the button
-		self.submit.bind(on_press=self.press)
-		self.top_grid.add_widget(self.submit)
-
-	def press(self, instance):
+	def press(self):
 		download = self.download.text
 
 		url = download
@@ -63,7 +30,7 @@ class MyGridLayout(GridLayout):
 		ffmpeg = ('ffmpeg -i %s ' % mp4 + mp3)
 		subprocess.call(ffmpeg, shell=True)
 		os.remove(name + '.mp4')
-		self.add_widget(Label(text=f'Finished Download!'))
+		#self.add_widget(Label(text=f'Finished Download!'))
 
 
 		# Clear the input boxes

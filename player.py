1d20e44cd2713e5f75d9f4e0435bde3aac4976ce
mihail.boshkilov <86418524+Migumagunata@users.noreply.github.com>
2022-06-19 01:44:26 +0300

    Everything work
    Volume slider working, progress bar working, auto switching song at the end of the current one unless it is not the last song on the list and not crashing after you entered wrong URL.

--- player.py
+++ player.py
@@ -1,6 +1,9 @@
+from logging import logProcesses
 import os
 import random
+from textwrap import shorten
 import time
+from turtle import position
 import kivy
 from pytube import YouTube
 import subprocess
@@ -12,20 +15,32 @@ from kivymd.uix.button import MDIconButton
 from kivymd.app import MDApp
 from kivy.core.audio import SoundLoader
 from kivy.core.window import Window
+
 from kivy.clock import Clock
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.textinput import TextInput
+from kivy.uix.slider import Slider
+
 
 Window.size = (400, 600)
 global song_number
 song_number = 0
+global seconds_elapsed
+seconds_elapsed=0
+global loop_control
+loop_control=False
+global shuffle_control
+shuffle_control=False
+
+global Flag_stop_manual
+Flag_stop_manual = False
 
 class MusicApp(MDApp):
     def build(self):
 
         layout = MDRelativeLayout(md_bg_color = [50/255, 112/255, 175/255, 1])
 
-        self.music_dir = "C:/Users/lubol/OneDrive/Desktop/Rythm33"
+        self.music_dir = "C:/Users/Misho/Documents/daskalo/Programirane 11 klas/proekt/Rhythm33-main"
 
         self.music_files = os.listdir(self.music_dir)
 
@@ -51,9 +66,14 @@ class MusicApp(MDApp):
                                   size_hint_x = None,
                                   width = 220)
 
-        self.songLabel = Label(pos_hint={'center_x':0.5, 'center_y':0.56},
-                               size_hint = (1,1),
-                               font_size = 18)
+        self.songLabel = Label(pos_hint={'center_x':0.5, 'center_y':0.66},
+                               size_hint = (200,135),
+                               font_size = 15,
+                               text_size = (200,135),
+                               halign = 'center',
+                            #    shorten = True,
+                              split_str = "_"
+                            )
 
         self.songImage = Image(pos_hint = {'center_x':0.5, 'center_y':0.35},
                                size_hint = (0.40, 0.27))
@@ -73,13 +93,23 @@ class MusicApp(MDApp):
                                        pos_hint = {'center_x':0.5,'center_y':0.12},
                                        size_hint = (.8,.75),)
 
+        self.slider = Slider(min=0,
+                             max=1,
+                             step=0.1,
+                             orientation = 'vertical',
+                             value = 1,
+                             pos_hint = {'center_x':0.1,'center_y':0.5},
+                             size_hint = (.1,.5),
+                             on_touch_move =self.vol,
+                             on_touch_up = self.vol)
+
         self.playbutton = MDIconButton(pos_hint={'center_x':0.425,'center_y':0.05},
                                        icon = 'play.png',
                                        on_press = self.playaudio)
 
         self.stopbutton = MDIconButton(pos_hint={'center_x':0.575,'center_y':0.05},
                                        icon = 'stop.png',
-                                       on_press = self.stopaudio, disabled = True)
+                                       on_press = self.stopaudio)
 
         self.nextbutton = MDIconButton(pos_hint={'center_x':0.725,'center_y':0.05},
                                        icon = 'next.png',
@@ -97,7 +127,7 @@ class MusicApp(MDApp):
                                        icon='loop.png',
                                        on_press = self.loop)
 
-
+        layout.add_widget(self.slider)
         layout.add_widget(self.playbutton)
         layout.add_widget(self.stopbutton)
         layout.add_widget(self.songLabel)
@@ -113,101 +143,175 @@ class MusicApp(MDApp):
         layout.add_widget(self.dtext)
         layout.add_widget(self.submit)
 
-        Clock.schedule_once(self.playaudio)
+        self.song_title = self.song_list[song_number]
+        self.songLabel.text = "=== Stopped ==="
+        Clock.schedule_once(self.song_load)
 
 
         return layout
 
-    def playaudio(self, obj):
-        self.playbutton.disabled = True
-        self.stopbutton.disabled = False
-        self.song_title = self.song_list[song_number]
+    def song_load(self, obj):
+
         self.sound = SoundLoader.load('{}/{}'.format(self.music_dir,self.song_title))
+        self.songImage.source = "background.png"
+        self.sound.on_stop = self.song_end
+        self.sound.volume = self.slider.value
+
+    def vol(self, touch, obj):
+        self.sound.volume = self.slider.value
+
+    def playaudio(self, obj):
 
         self.songLabel.text = "=== Playing: " + self.song_title[0:-4] + " ==="
-        self.songImage.source = "background.png"
+        global Flag_stop_manual
+        Flag_stop_manual = True
 
         self.sound.play()
-        self.progressbarEvent = Clock.schedule_interval(self.updatepbar,self.sound.length/100)
-        self.setttimeEvent = Clock.schedule_interval(self.settime,1)
-    def stopaudio(self, obj):
-        self.playbutton.disabled = False
-        self.stopbutton.disabled = True
-        self.sound.stop()
-        self.songLabel.text = "=== Stopped ==="
-        self.progressbarEvent.cancel()
-        self.setttimeEvent.cancel()
+        Flag_stop_manual = False
+
         self.progressbar.value = 0
+        global seconds_elapsed
+        seconds_elapsed = 0
         self.currenttime.text = "00:00"
-        self.totaltime.text = "00:00"
-        self.songImage.source = "pause.png"
-    def nextsong(self, obj):
+        total_time = time.strftime('%M:%S', time.gmtime(self.sound.length))
+        self.totaltime.text = total_time
+        self.progressbarEvent = Clock.schedule_interval(self.updatepbar,self.sound.length/100)
+        self.setttimeEvent = Clock.schedule_interval(self.settime,0.5)
+
+    def stopaudio(self, obj):
+
+        global Flag_stop_manual
+        Flag_stop_manual = True
+
         self.sound.stop()
+        Flag_stop_manual = False
+        self.songLabel.text = "=== Stopped ==="
+        
+
+
+    def nextsong(self, obj):
+
+        global Flag_stop_manual
+        Flag_stop_manual = True
+
+        is_playing=self.sound.state=="play"
+        self.sound.stop()
+        Flag_stop_manual = False
         global song_number
         song_number = song_number + 1
+
         if song_number == self.song_count:
             song_number = 0
         self.song_title = self.song_list[song_number]
-        self.sound = SoundLoader.load('{}/{}'.format(self.music_dir,self.song_title))
+        self.song_load(obj)
+        if(is_playing==True):
+            self.playaudio(obj)
 
-        self.songLabel.text = "=== Playing: " + self.song_title[0:-4] + " ==="
-        self.songImage.source = "background.png"
-
-        self.sound.play()
-        self.progressbarEvent = Clock.schedule_interval(self.updatepbar,self.sound.length/100)
-        self.setttimeEvent = Clock.schedule_interval(self.settime,1)
     def prevsong(self, obj):
+
+        global Flag_stop_manual
+        Flag_stop_manual = True
+        is_playing=self.sound.state=="play"
         self.sound.stop()
+        Flag_stop_manual = False
+        
         global song_number
         song_number = song_number - 1
         if song_number == -1:
             song_number = self.song_count - 1
         self.song_title = self.song_list[song_number]
-        self.sound = SoundLoader.load('{}/{}'.format(self.music_dir,self.song_title))
+        self.song_load(obj)
 
-        self.songLabel.text = "=== Playing: " + self.song_title[0:-4] + " ==="
-        self.songImage.source = "background.png"
+        if(is_playing==True):
+            self.playaudio(obj)
 
-        self.sound.play()
-        self.progressbarEvent = Clock.schedule_interval(self.updatepbar,self.sound.length/100)
-        self.setttimeEvent = Clock.schedule_interval(self.settime,1)
     def shuffle(self, obj):
-        self.sound.stop()
-        self.song_title = self.song_list[random.randrange(0, self.song_count)]
-        self.sound = SoundLoader.load('{}/{}'.format(self.music_dir, self.song_title))
 
-        self.songLabel.text = "=== Playing: " + self.song_title[0:-4] + " ==="
-        self.songImage.source = "background.png"
+        global shuffle_control
+        shuffle_control= not shuffle_control
 
-        self.sound.play()
-        self.progressbarEvent = Clock.schedule_interval(self.updatepbar, self.sound.length / 100)
-        self.setttimeEvent = Clock.schedule_interval(self.settime, 1)
     def loop(self, obj):
-        pass
+
+        global loop_control
+        loop_control= not loop_control
+
+
+    def song_end(self):
+
+        self.progressbarEvent.cancel()
+        self.setttimeEvent.cancel()
+        self.progressbar.value = 0
+        global seconds_elapsed
+        seconds_elapsed = 0
+        self.currenttime.text = "00:00"
+        self.totaltime.text = "00:00"
+
+        print("POOTIS")
+        global Flag_stop_manual
+        if(Flag_stop_manual == True):
+            Flag_stop_manual = False
+            return
+
+        global loop_control
+        if(loop_control==True):
+            self.playaudio(self)
+            return
+
+        global shuffle_control
+        global song_number
+        if(shuffle_control==True):
+            song_number = random.randint(0, self.song_count-1)
+        else:
+            song_number = song_number + 1
+            if song_number == self.song_count:
+                song_number = song_number - 1
+                self.songLabel.text = "=== Stopped ==="
+                return
+        self.song_title = self.song_list[song_number]
+        self.song_load(self)
+        self.playaudio(self)
+
+
+
+
+
+
     def enter(self, obj):
         download = self.download.text
 
         url = download
-        yt = YouTube(url)
+        try:
+            yt = YouTube(url)
+        except:
+            print("YOU FUCKING IDIOT enter a fucking ")
+            return
+
         name = yt.title.replace(' ', '_')
         only_audio_streams = (yt.streams.filter(only_audio=True))
-        destination = "C:/Users/lubol/OneDrive/Desktop"
+        destination = "\"" + self.music_dir + "/"
         stream = yt.streams.get_audio_only('mp4')
-        stream.download(filename=name + '.mp4')
+        stream.download(filename = name + '.mp4')
         mp4 = name + '.mp4'
-        mp3 = name + '.mp3'
+        mp3 = destination + name + '.mp3\"'
         ffmpeg = ('ffmpeg -i %s ' % mp4 + mp3)
         subprocess.call(ffmpeg, shell=True)
         os.remove(name + '.mp4')
         self.download.text = " "
+
+        music_files_new = os.listdir(self.music_dir)
+        song_list_new = [x for x in music_files_new if x.endswith(('mp3')) and x not in self.song_list]
+        for x in song_list_new:
+            self.song_list.append(x)
+        self.song_count = len(self.song_list)
+
     def updatepbar(self, value):
         if self.progressbar.value < 100:
             self.progressbar.value +=1
     def settime(self, t):
-        current_time = time.strftime('%M:%S', time.gmtime(self.progressbar.value*self.sound.length/100))
-        total_time = time.strftime('%M:%S', time.gmtime(self.sound.length))
+        global seconds_elapsed
+        current_time = time.strftime('%M:%S', time.gmtime(seconds_elapsed))
         self.currenttime.text = current_time
-        self.totaltime.text = total_time
+        seconds_elapsed = seconds_elapsed + t
 
 if __name__ == '__main__':
 	MusicApp().run()
